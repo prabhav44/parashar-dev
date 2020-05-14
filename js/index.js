91437e4234c5d3775afa5085931c5704db49a99d
Prabhav Parashar <prabhav.parashar@gmail.com>
2020-05-13 23:26:41 -0400

    added multicloud particle effect

--- js/index.js
+++ js/index.js
@@ -12,4 +12,12 @@ $(document).ready(() => {
 
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
-  });
+});
+
+particlesJS.load('particles-js-2', 'assets/particles2.json', function() {
+    console.log('callback - particles.js-2 config loaded');
+});
+
+particlesJS.load('particles-js-3', 'assets/particles3.json', function() {
+    console.log('callback - particles.js-3 config loaded');
+});
