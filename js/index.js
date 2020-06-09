b7bbfa83575abd5d056735858f03da03348b892e
Prabhav Parashar <prabhav.parashar@gmail.com>
2020-06-08 23:42:59 -0400

    added fetch function based submit for sending in quote request

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
+function sendQuoteRequest(emailAddress, fullname, phoneNumber, companyName, commentSection, buttonID) {
+    let url = "https://atinvzrwth.execute-api.us-east-1.amazonaws.com/v1/request-quote"
+    let button = document.getElementById(buttonID)
+
+    var request_body = {
+        'emailAddress': '',
+        'fullname': '',
+        'phoneNumber': '',
+        'companyName': '',
+        'commentSection': ''
+    }
+
+    button.innerHTML = "Submitting..."
+    button.disabled = true
+
+    request_body.emailAddress = document.getElementById(emailAddress).value
+    request_body.fullname = document.getElementById(fullname).value
+    request_body.phoneNumber = document.getElementById(phoneNumber).value
+    request_body.companyName = document.getElementById(companyName).value
+    request_body.commentSection = document.getElementById(commentSection).value
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request_body)
+    })
+    .then((response) => {return response.json()})
+    .then((data) => {
+        console.log(data)
+        button.innerHTML = "Submitted!"
+    })
+}
+
 $(document).ready(() => {
     var htmlDict = {
         "/": "home.html",
